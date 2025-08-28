07cd618a6b10748ad9559f41180008f5e806c507
mzee-flutter <mudair.khan37@gmail.com>
2025-08-28 15:38:31 +0400

    this is the beginner friendly backend project code that cover the basic concept

---
+++ jwt_authentication/protected_user_routes.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Depends, APIRouter
+from database import get_db
+from fastapi.security import OAuth2PasswordBearer
+from . import schemas, models, utils
+
+
+
+user_router= APIRouter(prefix="/users", tags= ["Users"])
+oauth2_schemes= OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(token: str=(Depends(oauth2_schemes)), db: Session= Depends(get_db)):
+    payload= utils.decode_token(token)
+    if payload is None:
+        raise HTTPException(detail="Invalid or expire token")
+    
+    user = db.query(models.User).filter(models.User.id == int(payload["sub"])).first()
+    if not user:
+        raise HTTPException(detail="user not found")
+    return user
+
+
+@user_router.get('/me', response_model=schemas.showUser)
+def get_me(current_user: models.User= Depends(get_current_user)):
+    return current_user
