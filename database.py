daec27aae4090120b8815a90130ee50ae17a304a
mzee-flutter <mudair.khan37@gmail.com>
2025-08-28 11:20:58 +0400

    some updates in this file for adding more routes for the authentication and authorization

--- database.py
+++ database.py
@@ -8,6 +8,10 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL= "postgresql://mudasir:mudasir777@localhost:5432/crudDB"
 
+SECRETE_KEY= "This is going to the secrete key for the jwt token generation"
+ALGORITHM= "HS256"
+ACCESS_TOKEN_EXPIRE_TIME= 60
+
 
 
 engine= create_engine(SQLALCHEMY_DATABASE_URL)
