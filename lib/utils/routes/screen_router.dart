3a3a0b365a46c42b44d74323ebb16f0afc7c6493
mzee-flutter <mudair.khan37@gmail.com>
2025-08-29 16:29:17 +0400

    here is the router that route the screens

--- lib/utils/routes/screen_router.dart
+++ lib/utils/routes/screen_router.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_postgres/utils/routes/routes_name.dart';
 import 'package:flutter_postgres/view/home_screen_view.dart';
+import 'package:flutter_postgres/view/login_screen_view.dart';
+import 'package:flutter_postgres/view/register_screen_view.dart';
 
 class ScreenRouter {
   static MaterialPageRoute generateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case RoutesName.homeScreen:
         return MaterialPageRoute(builder: (_) => HomeScreen());
+      case RoutesName.loginScreen:
+        return MaterialPageRoute(builder: (_) => LoginScreenView());
+      case RoutesName.registerScreen:
+        return MaterialPageRoute(builder: (_) => RegisterScreenView());
 
       default:
         return MaterialPageRoute(
