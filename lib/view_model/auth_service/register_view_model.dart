3aaac84a86dd6b164c558880662d5980cd3cbde5
mzee-flutter <mudair.khan37@gmail.com>
2025-08-30 22:23:36 +0400

    this is register view model class the provider

--- lib/view_model/auth_service/register_view_model.dart
+++ lib/view_model/auth_service/register_view_model.dart
@@ -10,6 +10,9 @@ class RegisterViewModel with ChangeNotifier {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
   Future<AuthModel?> registerUser(BuildContext context) async {
     final name = nameController.text.trim();
     final email = emailController.text.trim();
@@ -28,9 +31,13 @@ class RegisterViewModel with ChangeNotifier {
       password: password,
     );
 
+    _isLoading = true;
+    notifyListeners();
     try {
       final user = await _registerRepo.registerUser(newUser);
 
+      _isLoading = false;
+      notifyListeners();
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('User ${user.name} registered successfully')),
       );
@@ -43,6 +50,9 @@ class RegisterViewModel with ChangeNotifier {
         const SnackBar(content: Text('Registration failed. Try again.')),
       );
       return null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
     }
   }
 }
