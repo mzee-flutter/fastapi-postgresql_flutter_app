c84d88e878a1489c09202eaaabed364bbb7362b2
mzee-flutter <mudair.khan37@gmail.com>
2025-08-30 22:19:50 +0400

    this is login view model with provider state managment

--- lib/view_model/auth_service/login_view_model.dart
+++ lib/view_model/auth_service/login_view_model.dart
@@ -1,3 +1,46 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_postgres/models/login_request_model.dart';
+import 'package:flutter_postgres/repository/auth_repo/login_repo.dart';
 
-class LoginViewModel with ChangeNotifier {}
+import '../../models/token_model.dart';
+
+class LoginViewModel with ChangeNotifier {
+  final _loginRepo = LoginRepository();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  Future<TokenModel?> loginUser(context) async {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('All fields are required ')));
+    }
+
+    final existingUser = LoginRequestModel(email: email, password: password);
+    _isLoading = true;
+    notifyListeners();
+    try {
+      final user = await _loginRepo.loginUser(existingUser);
+
+      _isLoading = false;
+      notifyListeners();
+      return user;
+    } catch (e) {
+      debugPrint('Error in LoginViewModel: ${e.toString()}');
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Login Failed. Try Again!')));
+      return null;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
