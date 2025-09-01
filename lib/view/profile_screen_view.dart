18589da2c22f78b14931e90275fa1a10ac51f241
mzee-flutter <mudair.khan37@gmail.com>
2025-09-01 10:48:09 +0400

    this is the screen where we display the user information

--- lib/view/profile_screen_view.dart
+++ lib/view/profile_screen_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../view_model/auth_service/login_view_model.dart';
 import '../view_model/auth_service/register_view_model.dart';
 
 class ProfileScreenView extends StatelessWidget {
@@ -8,12 +9,24 @@ class ProfileScreenView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Consumer<RegisterViewModel>(
-        builder: (context, registerVM, child) {
+      body: Consumer2<RegisterViewModel, LoginViewModel>(
+        builder: (context, registerVM, loginVM, child) {
           return Column(
             mainAxisAlignment: MainAxisAlignment.center,
 
             children: [
+              Material(
+                color: Colors.grey.shade300,
+                child: ListTile(
+                  title: Text(
+                    'ACCESS-TOKEN: ${loginVM.token?.accessToken.toString() ?? 'Error'}',
+                  ),
+                  subtitle: Text(
+                    'TOKEN-TYPE: ${loginVM.token?.tokenType.toString() ?? 'Unknown'}',
+                  ),
+                ),
+              ),
+
               Center(
                 child: Container(
                   height: 100,
@@ -28,9 +41,21 @@ class ProfileScreenView extends StatelessWidget {
               Material(
                 color: Colors.grey.shade300,
                 child: ListTile(
-                  leading: Text(registerVM.user?.id.toString() ?? '1'),
-                  title: Text(registerVM.user?.name ?? 'Unknown'),
-                  subtitle: Text(registerVM.user?.email ?? 'example@gmail.com'),
+                  leading: Text(
+                    registerVM.user?.id.toString() ??
+                        loginVM.loggedInUserInfo?.id.toString() ??
+                        '1',
+                  ),
+                  title: Text(
+                    registerVM.user?.name ??
+                        loginVM.loggedInUserInfo?.name ??
+                        'Unknown',
+                  ),
+                  subtitle: Text(
+                    registerVM.user?.email ??
+                        loginVM.loggedInUserInfo?.email ??
+                        'example@gmail.com',
+                  ),
                 ),
               ),
             ],
