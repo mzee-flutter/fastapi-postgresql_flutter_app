618f79bfd341aa5fd010c26c3bb4dcc3f792e6a0
mzee-flutter <mudair.khan37@gmail.com>
2025-09-02 22:59:01 +0400

    here i add the create refresh token function and some helper function that convert the plain text into hashed string

--- jwt_authentication/utils.py
+++ jwt_authentication/utils.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+import secrets, hashlib
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, UTC
 from database import ACCESS_TOKEN_EXPIRE_TIME, SECRETE_KEY, ALGORITHM
@@ -8,18 +9,34 @@ from database import ACCESS_TOKEN_EXPIRE_TIME, SECRETE_KEY, ALGORITHM
 pwd_context= CryptContext(schemes=["bcrypt"], deprecated= "auto")
 
 
-def hash_password(password:str):
+def hash_password(password:str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(password, hash_password):
+def verify_password(password, hash_password) -> bool:
     return pwd_context.verify(password, hash_password)
+
+
+#This is the hashlib.sha256 calculate the hash of the given string
+#s.encode() convert into bytes because sha256 operates on bytes and finally return as String of hexadecimal digits.
+def _sha256(input:str) ->str:
+    return hashlib.sha256(input.encode()).hexdigest()
 
 def create_token(data: dict):
     to_encode= data.copy()
     expire= datetime.now(UTC) + timedelta(minutes= ACCESS_TOKEN_EXPIRE_TIME)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode, SECRETE_KEY,algorithm= ALGORITHM )
+
+
+
+
+#This function return two tokens that is 
+#One raw_token that is unhashed_token
+#Second which is return by the _sha265 helper-function that is hashed_token
+def create_refresh_token():
+    raw_token= secrets.token_urlsafe(64)
+    return raw_token, _sha256(raw_token) 
 
 
 def decode_token(token:str):
