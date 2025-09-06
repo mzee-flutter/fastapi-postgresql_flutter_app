e4b9a7d1245fef1d4b8bae8608ee0c5e1ac6fbd4
mzee-flutter <mudair.khan37@gmail.com>
2025-09-06 14:10:48 +0400

    here is the refresh  repo and refresh view model class that is response to refresh the access token

---
+++ lib/repository/auth_repo/refresh_access_token_repo.dart
@@ -0,0 +1,32 @@
+import 'dart:js_interop';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_postgres/data/base_api_services.dart';
+import 'package:flutter_postgres/data/network_api_services.dart';
+import 'package:flutter_postgres/resources/api_urls.dart';
+import 'package:flutter_postgres/view_model/token_storage_service/token_storage_service.dart';
+
+class RefreshAccessTokenRepo {
+  final BaseApiServices _services = NetworkApiServices();
+  final TokenStorageService _tokenStorage = TokenStorageService();
+
+  Future<void> getFreshAccessToken(String? token) async {
+    final headers = {"Content-Type": "application/json"};
+
+    try {
+      final response = await _services.getPostApiRequest(
+        ApiURls.refreshAccessToken,
+        headers,
+        {"refresh_token": token},
+      );
+
+      final accessToken = response["access_token"];
+      final refreshToken = response["refresh_token"];
+
+      await _tokenStorage.saveToken(accessToken, refreshToken);
+    } catch (e) {
+      debugPrint("RefreshAccessTokenRepo: $e");
+      rethrow;
+    }
+  }
+}
